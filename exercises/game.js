b499fb43de8c789b2a005121d13ea50f1a53c50a
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-28 15:12:47 -0400

    update

--- exercises/game.js
+++ exercises/game.js
@@ -3,14 +3,22 @@
 // = assignment, put something in the box
 // == comparison, compare what is on the left to what is on the right, returns boolean (true or false)
 
+//Data Types:
 //string = ""
 //number = 3
 //boolean = true, false (comparison course.CourseId == "PROG100")
 //object = { }
 //array = [ ]
-   
-let dog = {
 
-}
+// Data types?
+// hat is an object
+// hat.color is a property, that property has a data type of string
+// hat.size is a property, that property has a data type of number
+let hat = {
+  color: "blue",
+  style: "bucket",
+  size: 12,
+};
 
-let x 
+
+const teamsDropdown = document.getElementById("teamsDropdown");

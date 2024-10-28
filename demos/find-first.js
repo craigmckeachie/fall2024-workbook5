ab8d304193dd4073818671013754dc8388f1ff27
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-28 10:45:08 -0400

    3 flavors of find

--- demos/find-first.js
+++ demos/find-first.js
@@ -2,6 +2,8 @@
 
 let numbers = [7, 9, 64, 60, 12, 13, 65, 62];
 
+//example 1: define function with condition separately
+
 function isOver60(arrayValue) {
   if (arrayValue > 60) {
     return true;
@@ -16,3 +18,21 @@ function isOver60(arrayValue) {
 // and undefined otherwise.
 let firstValueOver60 = numbers.find(isOver60);
 console.log(firstValueOver60);
+
+//example 2: inline function
+let firstValueOver60v2 = numbers.find(function isOver60(arrayValue) {
+  if (arrayValue > 60) {
+    return true;
+  } else {
+    return false;
+  }
+  //OR shorter version
+  //   return arrayValue > 60;
+});
+console.log(firstValueOver60v2);
+
+// example 3: inline, anonymous function (using shorter conditional)
+let firstValueOver60v3 = numbers.find(function (arrayValue) {
+  return arrayValue > 60;
+});
+console.log(firstValueOver60v3);

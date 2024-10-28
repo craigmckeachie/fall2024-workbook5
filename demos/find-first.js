ccb00e40240735d5e6485030b85a02fce98601e9
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-28 10:33:30 -0400

    find

--- demos/find-first.js
+++ demos/find-first.js
@@ -8,8 +8,11 @@ function isOver60(arrayValue) {
   } else {
     return false;
   }
+  //OR shorter version
+  //   return arrayValue > 60;
 }
 
-
+//.find method (function) Returns the value of the first element in the array where condition in the function is true,
+// and undefined otherwise.
 let firstValueOver60 = numbers.find(isOver60);
 console.log(firstValueOver60);

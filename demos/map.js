05753ed45b4b53f48ac0a5c4dbfd0536875f0c2a
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-29 13:35:10 -0400

    map

---
+++ demos/map.js
@@ -0,0 +1,36 @@
+"use strict";
+
+//array of objects that represent a kid
+let kids = [
+  { first: "Natalie", last: "Plyers" },
+  { first: "Brittany", last: "Ray" },
+  { first: "Zachary", last: "Westly" },
+];
+
+//array of string that represent the fullname of a kid
+let kidNames = kids.map(function (kid) {
+  return kid.first + " " + kid.last;
+});
+
+console.log(kidNames);
+
+//Exercise is fun.
+// function (){
+
+// }
+
+// function(){
+
+// }
+
+// function(){
+
+// }
+
+// ()=> {
+//
+// }
+
+// ()=> {}
+
+// ()=> {}

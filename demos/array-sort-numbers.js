ba92b49cdcdc91978c4059c3100b06ac1e06f945
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-30 12:00:23 -0400

    array sort numbers

--- demos/array-sort-numbers.js
+++ demos/array-sort-numbers.js
@@ -1,3 +1,15 @@
 "use strict";
 
 const randomNumbers = [298, 754, 855, 413, 579, 932, 533, 64, 464, 344, 65, 636, 439, 903, 606, 205, 971, 743, 623];
+
+randomNumbers.sort(function (a, b) {
+  return a - b;
+  //OR LONG VERSION
+  //   if (a < b) return -1;  // right order
+  //   else if (a == b) return 0; // same values
+  //   else return 1; // swap, wrong order
+});
+
+randomNumbers.sort((a, b) => a - b);
+
+console.log(randomNumbers);

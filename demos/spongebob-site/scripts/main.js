447a18700e4d736885a3bc7c5a945a06ead6815c
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-31 14:19:22 -0400

    add friend group column

--- demos/spongebob-site/scripts/main.js
+++ demos/spongebob-site/scripts/main.js
@@ -6,101 +6,115 @@ const spongebobCharacters = [
     species: "Sea Sponge",
     occupation: "Fry Cook",
     favoriteActivity: "Jellyfishing",
+    friendGroup: "Main Characters",
   },
   {
     name: "Patrick Star",
     species: "Starfish",
     occupation: "Unemployed",
     favoriteActivity: "Eating and sleeping",
+    friendGroup: "Main Characters",
   },
   {
     name: "Squidward Tentacles",
     species: "Octopus",
     occupation: "Cashier",
     favoriteActivity: "Playing the clarinet",
+    friendGroup: "Main Characters",
   },
   {
     name: "Mr. Krabs",
     species: "Crab",
     occupation: "Restaurant Owner",
     favoriteActivity: "Counting money",
+    friendGroup: "Bikini Bottom Business Owners",
   },
   {
     name: "Sandy Cheeks",
     species: "Squirrel",
     occupation: "Scientist/Inventor",
     favoriteActivity: "Karate and science experiments",
+    friendGroup: "Main Characters",
   },
   {
     name: "Plankton",
     species: "Plankton",
     occupation: "Restaurant Owner",
     favoriteActivity: "Stealing the Krabby Patty formula",
+    friendGroup: "Bikini Bottom Business Owners",
   },
   {
     name: "Mrs. Puff",
     species: "Pufferfish",
     occupation: "Boating School Instructor",
     favoriteActivity: "Teaching and avoiding SpongeBob",
+    friendGroup: "Educators",
   },
   {
     name: "Gary the Snail",
     species: "Snail",
     occupation: "Pet",
     favoriteActivity: "Meowing and eating",
+    friendGroup: "Main Characters",
   },
   {
     name: "Larry the Lobster",
     species: "Lobster",
     occupation: "Lifeguard",
     favoriteActivity: "Weightlifting and staying fit",
+    friendGroup: "Sports and Fitness Enthusiasts",
   },
   {
     name: "Karen Plankton",
     species: "Computer",
     occupation: "Plankton's Wife/Assistant",
     favoriteActivity: "Calculating and assisting Plankton",
+    friendGroup: "Bikini Bottom Business Owners",
   },
   {
     name: "Pearl Krabs",
     species: "Whale",
     occupation: "Student",
     favoriteActivity: "Shopping and hanging out with friends",
+    friendGroup: "Teenagers of Bikini Bottom",
   },
   {
     name: "Mermaid Man",
     species: "Human",
     occupation: "Retired Superhero",
     favoriteActivity: "Fighting crime",
+    friendGroup: "Superheroes",
   },
   {
     name: "Barnacle Boy",
     species: "Human",
     occupation: "Retired Superhero Sidekick",
     favoriteActivity: "Being grumpy",
+    friendGroup: "Superheroes",
   },
   {
     name: "Flying Dutchman",
     species: "Ghost",
     occupation: "Ghostly Pirate",
     favoriteActivity: "Scaring people and collecting souls",
+    friendGroup: "Otherworldly Characters",
   },
   {
     name: "Bubble Buddy",
     species: "Bubble",
+    occupation: "Friend of SpongeBob",
+    favoriteActivity: "Floating around and having fun",
+    friendGroup: "Main Characters",
   },
 ];
-
 
 // let charactersTBody = document.getElementById("charactersTBody");
 let charactersTBody = document.querySelector("#charactersTBody");
 console.log(charactersTBody);
 
-
 for (const character of spongebobCharacters) {
-
   let tr = document.createElement("tr");
-  charactersTBody.appendChild(tr)
+  charactersTBody.appendChild(tr);
 
   let td1 = document.createElement("td");
   td1.innerText = character.name;
@@ -117,8 +131,8 @@ for (const character of spongebobCharacters) {
   let td4 = document.createElement("td");
   td4.innerText = character.favoriteActivity;
   tr.appendChild(td4);
-  
- 
 
+  let td5 = document.createElement("td");
+  td5.innerText = character.friendGroup;
+  tr.appendChild(td5);
 }
-
